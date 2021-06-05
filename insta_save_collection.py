271fd9667352b56f2be859e60ff45f1d2c752012
sidharth72 <sidharthan261@gmail.com>
2021-06-05 09:51:29 -0700

    Instagram

---
+++ insta_save_collection.py
@@ -0,0 +1,19 @@
+import instaloader
+from datas import username, password
+
+def insta_save():
+	
+	loader = instaloader.Instaloader()
+	loader.load_session_from_file(username)
+	profile = instaloader.Profile.from_username(loader.context,username)
+
+	arr = []
+	for saved_data in profile.get_saved_posts():
+		arr.append(saved_data)
+
+	loader.download_post(arr[0],'my_saved_collections')
+
+	insta_save()
+
+
+insta_save()
